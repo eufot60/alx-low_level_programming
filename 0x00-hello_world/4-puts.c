6c3369b3f8aa4b5fefbd18b58e7fa627ea96cc0f
eufot60 <eufot60@gmail.com>
2023-10-05 06:30:25 -0700

    gh

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *main - beginning
+ *return - must reurn 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
