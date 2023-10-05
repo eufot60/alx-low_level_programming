988caf1396c6876d5a5d7a4778778a0a3d9f7264
eufot60 <eufot60@gmail.com>
2023-10-05 07:20:21 -0700

    gh

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - starting point
+  *return: to be good (result)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
