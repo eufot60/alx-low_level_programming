8ecc03fbe2ca012591f6369063fcd10eda6be00d
eufot60 <eufot60@gmail.com>
2023-10-06 13:19:59 -0700

    d

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the letters.
+ *
+ * Return: Always 0 (execellent)
+ *
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
 
